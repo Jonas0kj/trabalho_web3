be98ae5e6596fdde036ce8da4184da555fa7e861
Jonas0kj <143147795+Jonas0kj@users.noreply.github.com>
2023-10-30 22:23:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+function ligarG() {
+  document.getElementById("b1").style.backgroundColor = "green";
+}
+
+function desligarG() {
+  document.getElementById("b1").style.backgroundColor = "red";
+}
+
+function ligarEP() {
+  document.getElementById("b5").style.backgroundColor = "green";
+}
+
+function desligarEP() {
+  document.getElementById("b5").style.backgroundColor = "red";
+}
+
+function ligarQ1() {
+  document.getElementById("b2").style.backgroundColor = "green";
+}
+
+function desligarQ1() {
+  document.getElementById("b2").style.backgroundColor = "red";
+}
+function ligarQ2() {
+  document.getElementById("b7").style.backgroundColor = "green";
+}
+
+function desligarQ2() {
+  document.getElementById("b7").style.backgroundColor = "red";
+}
+
+function ligarC() {
+  document.getElementById("b8").style.backgroundColor = "green";
+  document.getElementById("b4").style.backgroundColor = "green";
+}
+
+function desligarC() {
+  document.getElementById("b8").style.backgroundColor = "red";
+  document.getElementById("b4").style.backgroundColor = "red";
+}
+
+function ligarB() {
+  document.getElementById("b3").style.backgroundColor = "green";
+}
+
+function desligarB() {
+  document.getElementById("b3").style.backgroundColor = "red";
+}
+
+function ligarA() {
+  document.getElementById("b4").style.backgroundColor = "green";
+  document.getElementById("b8").style.backgroundColor = "green";
+}
+
+function desligarA() {
+  document.getElementById("b4").style.backgroundColor = "red";
+  document.getElementById("b8").style.backgroundColor = "red";
+}
+
+function ligarSE() {
+  document.getElementById("b6").style.backgroundColor = "green";
+}
+
+function desligarSE() {
+  document.getElementById("b6").style.backgroundColor = "red";
+}
